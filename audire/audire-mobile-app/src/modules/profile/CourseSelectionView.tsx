d8c496cfbf66e4aeb803fddb8b0210d2d06c6454
Mohammed Sameer PV <pvsameerpvs@gmail.com>
2023-12-18 12:03:07 +0530

    Fix: CourseSelectionView in app

--- audire/audire-mobile-app/src/modules/profile/CourseSelectionView.tsx
+++ audire/audire-mobile-app/src/modules/profile/CourseSelectionView.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { TouchableOpacity } from 'react-native';
 import { Link } from 'expo-router';
 import { Text, Box } from '@gluestack-ui/themed';
+import { useCourses } from '@learning-app/syllabus';
 
 const CourseSelectionView = () => {
   const handleCardPress = (course: unknown) => {
     // Handle card press logic here
     console.log(`Selected course: ${course}`);
   };
+  const { data: { data: courses } = { data: [] } } = useCourses();
 
   return (
     <Box
@@ -16,9 +18,7 @@ const CourseSelectionView = () => {
       w="$full"
       borderTopLeftRadius="$3xl"
       borderTopRightRadius="$3xl"
-      //Android
       elevation="$1.5"
-      // iOS
       shadowColor="$black"
       shadowOpacity="$40"
       shadowRadius="$8"
@@ -28,70 +28,37 @@ const CourseSelectionView = () => {
       }}
     >
       <Box flexDirection="column" alignItems="center" mt="$24">
-        <Box
-          maxWidth="$64"
-          borderColor="$black"
-          borderRadius="$lg"
-          borderWidth="$1"
-          my="$4"
-          backgroundColor="$white"
-          overflow="hidden"
-          sx={{
-            '@base': {
-              mx: '$5',
-            },
-            _dark: {
-              bg: '$backgroundDark900',
-              borderColor: '$borderDark800',
-            },
-          }}
-        >
-          <Link href="/" asChild>
-            <Box
-              m="$4"
-              w="$56"
-              h="$12"
-              // justifyContent="center"
-              alignItems="center"
-            >
-              <TouchableOpacity onPress={() => handleCardPress('CA')}>
-                <Text fontSize="$xl" fontWeight="bold">
-                  CA
-                </Text>
-              </TouchableOpacity>
-            </Box>
-          </Link>
-        </Box>
-        <Box
-          maxWidth="$64"
-          borderColor="#171717"
-          borderRadius="$lg"
-          borderWidth="$1"
-          my="$4"
-          backgroundColor="$white"
-          overflow="hidden"
-          sx={{
-            '@base': {
-              mx: '$5',
-            },
-          }}
-        >
-          <Link href="/" asChild>
-            <Box
-              m="$4"
-              w="$56"
-              h="$12"
-              // justifyContent="center"
-              alignItems="center"
-            >
-              <TouchableOpacity onPress={() => handleCardPress('CMA')}>
-                <Text fontSize="$xl" fontWeight="bold">
-                  CMA
-                </Text>
-              </TouchableOpacity>
-            </Box>
-          </Link>
-        </Box>
+        {courses.map((course) => (
+          <Box
+            key={course.id}
+            maxWidth="$64"
+            borderColor="$black"
+            borderRadius="$lg"
+            borderWidth="$1"
+            my="$4"
+            backgroundColor="$white"
+            overflow="hidden"
+            sx={{
+              '@base': {
+                mx: '$5',
+              },
+              _dark: {
+                bg: '$backgroundDark900',
+                borderColor: '$borderDark800',
+              },
+            }}
+          >
+            <Link href="/" asChild>
+              <Box m="$4" w="$56" h="$12" alignItems="center">
+                <TouchableOpacity onPress={() => handleCardPress(course.title)}>
+                  <Text fontSize="$xl" fontWeight="bold">
+                    {course.title}
+                  </Text>
+                </TouchableOpacity>
+              </Box>
+            </Link>
+          </Box>
+        ))}
       </Box>
     </Box>
   );
