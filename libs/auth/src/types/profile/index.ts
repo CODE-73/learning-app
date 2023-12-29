a36f1461ed80330b9043e1fe767650f9428dfc1a
Muhammed Rameez <rameez@REMI-MI-ULTRA>
2023-12-29 15:10:58 +0530

    fix: Couple of Build Errors

--- libs/auth/src/types/profile/index.ts
+++ libs/auth/src/types/profile/index.ts
@@ -1,4 +1,12 @@
 import { Database } from '@learning-app/supabase';
+import { CourseWithStages } from '@learning-app/syllabus';
 
 export type Profile = Database['public']['Tables']['Profile']['Row'];
 export type ProfileUpdate = Database['public']['Tables']['Profile']['Update'];
+
+export type ProfileExtended = Pick<
+  Profile,
+  'id' | 'firstName' | 'lastName' | 'mobile'
+> & {
+  optedCourse: CourseWithStages | null;
+};
--- libs/supabase/src/index.ts
+++ libs/supabase/src/index.ts
@@ -1,7 +1,6 @@
 export { type Database } from './supabase_types';
 export {
   NextSupabaseProvider,
-  ExpoSupabaseProvider,
   useSupabaseClient,
   type LearningAppSupabase,
 } from './providers';
