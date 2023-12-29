590ea668f21737604efaa2f16738c665a2528a41
Mohammed Sameer PV <pvsameerpvs@gmail.com>
2023-12-29 16:23:40 +0530

    feat: Init Audire Landing

---
+++ audire/audire-landing/pages/index.tsx
@@ -0,0 +1,9 @@
+export function Index() {
+  return (
+    <div className="h-screen w-screen flex items-center justify-center">
+      <div className="bg-rose-500	 w-10 h-10"></div>
+    </div>
+  );
+}
+
+export default Index;
---
+++ audire/audire-landing/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './modules/**/*.{ts,tsx}',
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './layouts/**/*.{ts,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
