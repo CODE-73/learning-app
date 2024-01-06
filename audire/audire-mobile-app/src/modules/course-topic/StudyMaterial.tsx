7860b5f5bd3528e1475dbf29e04ecdb13d5f1c03
Mohammed Sameer <pvsameerpvs@gmail.com>
2024-01-06 14:34:24 +0530

    fix: create pdf dowenload error

--- audire/audire-mobile-app/src/modules/course-topic/StudyMaterial.tsx
+++ audire/audire-mobile-app/src/modules/course-topic/StudyMaterial.tsx
@@ -1,19 +1,26 @@
-import { Box, Text } from '@gluestack-ui/themed';
-import React, { ComponentProps, FC, useState } from 'react';
-import { Platform, TouchableOpacity, Modal, View, Button } from 'react-native';
-import DownloadPDFIcon from '/assets/downloadPdf.svg';
-import * as WebBrowser from 'expo-web-browser';
+import {
+  Box,
+  Text,
+  Toast,
+  ToastDescription,
+  ToastTitle,
+  VStack,
+  useToast,
+} from '@gluestack-ui/themed';
 import { useR2Download } from '@learning-app/r2';
+import * as WebBrowser from 'expo-web-browser';
+import React, { ComponentProps, FC } from 'react';
+import { Platform, TouchableOpacity } from 'react-native';
+import DownloadPDFIcon from '/assets/downloadPdf.svg';
 
 type StudyMeterialProps = ComponentProps<typeof Box> & {
   r2Key: string;
 };
 
 const StudyMeterial: FC<StudyMeterialProps> = (props) => {
-  const [errorModalVisible, setErrorModalVisible] = useState(false);
   const { r2Key } = props;
   const { trigger: getURL, isMutating } = useR2Download();
-
+  const toast = useToast();
   const handlePdfOpen = async () => {
     try {
       const { url } = await getURL({
@@ -31,62 +38,51 @@ const StudyMeterial: FC<StudyMeterialProps> = (props) => {
         'An error occurred while getting the URL or opening the browser:',
         error
       );
-      setErrorModalVisible(true);
+      toast.show({
+        placement: 'bottom',
+        render: ({ id }) => {
+          const toastId = 'toast-' + id;
+          return (
+            <Toast nativeID={toastId} action="error" variant="accent">
+              <VStack space="xs">
+                <ToastTitle>Error downloading</ToastTitle>
+                <ToastDescription>something went wrong!..</ToastDescription>
+              </VStack>
+            </Toast>
+          );
+        },
+      });
     }
   };
 
-  const closeModal = () => {
-    setErrorModalVisible(false);
-  };
-
   return (
-    <>
-      <Box mx={17} borderRadius="$sm" my="$1" backgroundColor="#e5e5e5" py="$6">
-        <TouchableOpacity onPress={handlePdfOpen} disabled={isMutating}>
-          <Box
-            display="flex"
-            flexDirection="row"
-            justifyContent="space-between"
-            p="$4"
-          >
-            <Box pl="$4" mt="$2">
-              <Text
-                fontSize="$xl"
-                fontWeight="bold"
-                justifyContent="center"
-                alignItems="center"
-                numberOfLines={1}
-                ellipsizeMode="tail"
-              >
-                Study Material
-                {isMutating && ' (Loading...)'}
-              </Text>
-            </Box>
-            <Box pr="$4">
-              <DownloadPDFIcon />
-            </Box>
-          </Box>
-        </TouchableOpacity>
-      </Box>
-
-      <Modal
-        animationType="slide"
-        transparent={true}
-        visible={errorModalVisible}
-        onRequestClose={closeModal}
-      >
-        <View
-          style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
+    <Box mx={17} borderRadius="$sm" my="$1" backgroundColor="#e5e5e5" py="$6">
+      <TouchableOpacity onPress={handlePdfOpen} disabled={isMutating}>
+        <Box
+          display="flex"
+          flexDirection="row"
+          justifyContent="space-between"
+          p="$4"
         >
-          <View
-            style={{ padding: 20, backgroundColor: 'white', borderRadius: 10 }}
-          >
-            <Text>Error while loading</Text>
-            <Button title="Close" onPress={closeModal} />
-          </View>
-        </View>
-      </Modal>
-    </>
+          <Box pl="$4" mt="$2">
+            <Text
+              fontSize="$xl"
+              fontWeight="bold"
+              justifyContent="center"
+              alignItems="center"
+              numberOfLines={1}
+              ellipsizeMode="tail"
+            >
+              Study Material
+              {isMutating && ' (Loading...)'}
+            </Text>
+          </Box>
+          <Box pr="$4">
+            <DownloadPDFIcon />
+          </Box>
+        </Box>
+      </TouchableOpacity>
+    </Box>
   );
 };
 
