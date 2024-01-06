d5e78cfb6e9961af1d24450aa3cbad7f2bf90f3c
Muhammed Rameez <muhammedrameez1010@gmail.com>
2024-01-06 13:12:43 +0530

    feat : error on study material

--- audire/audire-mobile-app/src/modules/course-topic/StudyMaterial.tsx
+++ audire/audire-mobile-app/src/modules/course-topic/StudyMaterial.tsx
@@ -1,6 +1,6 @@
 import { Box, Text } from '@gluestack-ui/themed';
-import React, { ComponentProps, FC } from 'react';
-import { Platform, TouchableOpacity } from 'react-native';
+import React, { ComponentProps, FC, useState } from 'react';
+import { Platform, TouchableOpacity, Modal, View, Button } from 'react-native';
 import DownloadPDFIcon from '/assets/downloadPdf.svg';
 import * as WebBrowser from 'expo-web-browser';
 import { useR2Download } from '@learning-app/r2';
@@ -10,49 +10,83 @@ type StudyMeterialProps = ComponentProps<typeof Box> & {
 };
 
 const StudyMeterial: FC<StudyMeterialProps> = (props) => {
+  const [errorModalVisible, setErrorModalVisible] = useState(false);
   const { r2Key } = props;
   const { trigger: getURL, isMutating } = useR2Download();
 
   const handlePdfOpen = async () => {
-    const { url } = await getURL({
-      key: r2Key,
-    });
+    try {
+      const { url } = await getURL({
+        key: r2Key,
+      });
 
-    if (Platform.OS === 'web') {
-      window.open(url, '_blank');
-    } else {
-      WebBrowser.openBrowserAsync(url);
+      if (Platform.OS === 'web') {
+        window.open(url, '_blank');
+      } else {
+        WebBrowser.openBrowserAsync(url);
+      }
+    } catch (error) {
+      // Handle the error here
+      console.error(
+        'An error occurred while getting the URL or opening the browser:',
+        error
+      );
+      setErrorModalVisible(true);
     }
   };
 
+  const closeModal = () => {
+    setErrorModalVisible(false);
+  };
+
   return (
-    <Box mx={17} borderRadius="$sm" my="$1" backgroundColor="#e5e5e5" py="$6">
-      <TouchableOpacity onPress={handlePdfOpen} disabled={isMutating}>
-        <Box
-          display="flex"
-          flexDirection="row"
-          justifyContent="space-between"
-          p="$4"
+    <>
+      <Box mx={17} borderRadius="$sm" my="$1" backgroundColor="#e5e5e5" py="$6">
+        <TouchableOpacity onPress={handlePdfOpen} disabled={isMutating}>
+          <Box
+            display="flex"
+            flexDirection="row"
+            justifyContent="space-between"
+            p="$4"
+          >
+            <Box pl="$4" mt="$2">
+              <Text
+                fontSize="$xl"
+                fontWeight="bold"
+                justifyContent="center"
+                alignItems="center"
+                numberOfLines={1}
+                ellipsizeMode="tail"
+              >
+                Study Material
+                {isMutating && ' (Loading...)'}
+              </Text>
+            </Box>
+            <Box pr="$4">
+              <DownloadPDFIcon />
+            </Box>
+          </Box>
+        </TouchableOpacity>
+      </Box>
+
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={errorModalVisible}
+        onRequestClose={closeModal}
+      >
+        <View
+          style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
         >
-          <Box pl="$4" mt="$2">
-            <Text
-              fontSize="$xl"
-              fontWeight="bold"
-              justifyContent="center"
-              alignItems="center"
-              numberOfLines={1}
-              ellipsizeMode="tail"
-            >
-              Study Material
-              {isMutating && ' (Loading...)'}
-            </Text>
-          </Box>
-          <Box pr="$4">
-            <DownloadPDFIcon />
-          </Box>
-        </Box>
-      </TouchableOpacity>
-    </Box>
+          <View
+            style={{ padding: 20, backgroundColor: 'white', borderRadius: 10 }}
+          >
+            <Text>Error while loading</Text>
+            <Button title="Close" onPress={closeModal} />
+          </View>
+        </View>
+      </Modal>
+    </>
   );
 };
 
