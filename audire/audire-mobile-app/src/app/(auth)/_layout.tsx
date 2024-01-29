933be53748335ce712812f649b8888e826bbe0fd
Muhammed Rameez <muhammedrameez1010@gmail.com>
2024-01-29 16:30:17 +0530

    feat: Remove Top Bar In Mcq Page

--- audire/audire-mobile-app/src/app/(auth)/_layout.tsx
+++ audire/audire-mobile-app/src/app/(auth)/_layout.tsx
@@ -1,5 +1,5 @@
 import { UserProvider } from '@learning-app/auth';
-import { router } from 'expo-router';
+import { router, useSegments } from 'expo-router';
 import { Stack } from 'expo-router/stack';
 import { useState } from 'react';
 import Topbar from '../../modules/common/Topbar';
@@ -8,20 +8,28 @@ import Sidebar from '../../modules/common/sidebar/Sidebar';
 const MainLayout = () => {
   const [isSidebarOpen, setSidebarOpen] = useState(false);
 
+  const segments = useSegments();
+  console.log('aaaa', segments);
+  const hideTopbarSegments = ['(auth)/mcq-exams/[topic]'];
+
+  const hideTopbar = hideTopbarSegments.indexOf(segments.join('/')) >= 0;
+  console.log({ segments, hideTopbarSegments, hideTopbar });
+
   return (
     <UserProvider onUnauthorized={() => router.replace('/login')}>
-      <Topbar onToggleSidebar={() => setSidebarOpen(prev => !prev)} />
+      {!hideTopbar && (
+        <Topbar onToggleSidebar={() => setSidebarOpen((prev) => !prev)} />
+      )}
 
       <Sidebar isOpen={isSidebarOpen} setIsOpen={setSidebarOpen} />
       <Stack
-        initialRouteName='/'
+        initialRouteName="/"
         screenOptions={{
-          header: () => null
+          header: () => null,
         }}
       />
     </UserProvider>
   );
-
-}
+};
 
 export default MainLayout;
