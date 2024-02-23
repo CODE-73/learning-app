2fd79c7ab500a46dd874a4f43ee1777351a89d7d
rameez1010 <muhammedrameez1010@gmail.com>
2024-02-23 10:51:25 +0530

    feat: Subject Delection Bug

--- audire/audire-dashboard/pages/_app.tsx
+++ audire/audire-dashboard/pages/_app.tsx
@@ -6,6 +6,7 @@ import { NextPage } from 'next';
 import AuthorizedLayout from 'layouts/authorized/AuthorizedLayout';
 import { NextUIProvider } from '@nextui-org/react';
 import { NextSupabaseProvider } from '@learning-app/supabase';
+import { ToastContainer } from 'react-toastify';
 
 export type NextPageWithLayout<P = object, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -25,9 +26,11 @@ function MyApp({ Component, pageProps }: AppPropsWithLayout) {
       <Head>
         <title>Welcome to audire-dashboard!</title>
       </Head>
+
       <NextUIProvider>
         <main className="app">{getLayout(<Component {...pageProps} />)}</main>
       </NextUIProvider>
+      <ToastContainer />
     </NextSupabaseProvider>
   );
 }
